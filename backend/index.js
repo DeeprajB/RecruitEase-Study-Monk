1eb7ac5f2fee2f879222e5685ae400dc9431c4a8
DeeprajB <deeprajb012@gmail.com>
2023-07-08 11:56:19 +0530

    Added User Route & Schema.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 const express = require("express")
+const userRouter = require("./routes/userRoutes")
+const mongoose = require("mongoose")
+require('dotenv').config()
 
 const app = express()
+app.use("/users",userRouter)
 
-app.listen(5000, ()=>{
-    console.log('Server started on port 5000')
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    app.listen(4000, ()=>{
+        console.log('Server started on port 4000')
+    })
 })
+.catch((error) => {
+    console.log(error)
+})
+
